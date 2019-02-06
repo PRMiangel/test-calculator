9946b2cacecce177d6129b30c26f5a9a879aadd4
Miguel A. Pari Rojas <miangel-pr@outlook.com>
2019-02-06 00:52:22 -0400

    Add sample test using HTMLUnit

---
+++ selenium/java/src/test/java/lab/testautomation/calc/tests/HtmlUnitCalculatorTests.java
@@ -0,0 +1,62 @@
+package lab.testautomation.calc.tests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class HtmlUnitCalculatorTests {
+
+    private WebDriver wd;
+
+    @Before
+    public void runBeforeTest() {
+        wd = new HtmlUnitDriver(true);
+        wd.get("http://narulakeshav.github.io/calculator/");
+        wd.manage().window().maximize();
+    }
+
+    @After
+    public void runAfterTest() {
+        wd.quit();
+    }
+
+    @Test
+    public void additionTest() {
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("add")).click();
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("equals")).click();
+        Assert.assertEquals("8", wd.findElement(By.id("display")).getText());
+    }
+
+    @Test
+    public void subtractionTest() {
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("subtract")).click();
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("equals")).click();
+        Assert.assertEquals("0", wd.findElement(By.id("display")).getText());
+    }
+
+    @Test
+    public void multiplicationTest() {
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("multiply")).click();
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("equals")).click();
+        Assert.assertEquals("16", wd.findElement(By.id("display")).getText());
+    }
+
+    @Test
+    public void divisionTest() {
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("divide")).click();
+        wd.findElement(By.id("four")).click();
+        wd.findElement(By.id("equals")).click();
+        Assert.assertEquals("1", wd.findElement(By.id("display")).getText());
+    }
+}
